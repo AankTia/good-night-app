18471417157c3b26dd10fd4d6672901b1381147d
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 15:21:25 +0700

    Fix typo for has_many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
   has_many :followings, class_name: 'UserFollowing', foreign_key: 'follower_id', dependent: :destroy
-  has_mamy :followers, class_name: 'UserFollowing', foreign_key: 'Following_id', dependent: :destroy
+  has_many :followers, class_name: 'UserFollowing', foreign_key: 'Following_id', dependent: :destroy
 
   has_many :following_users, through: :followings, source: :following
   has_many :follower_users, through: :followers, source: :follower
