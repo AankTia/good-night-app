fd238c3d1b21fa8df0746a474956f012c524dc83
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 14:58:56 +0700

    Initial SleepRecord model

---
+++ app/models/sleep_record.rb
@@ -0,0 +1,27 @@
+class SleepRecord < ApplicationRecord
+  belongs_to :user
+
+  validates :sleep_time, presence: true
+  validate :wake_up_time_after_sleep_time, if: :wake_up_time?
+
+  scope :ordered_by_created_time, -> { order(:created_at) }
+  scope :completed, -> { where.not(wake_up_time: nil) }
+
+  before_save :calculate_duration
+
+  private
+
+  def wake_up_time_after_sleep_time
+    return unless wake_up_time && sleep_time
+
+    if wake_up_time <= sleep_time
+      errors.add(:wake_up_time, "must be after sleep time")
+    end
+  end
+
+  def calculate_duration
+    return unless wake_up_time && sleep_time
+
+    self.duration_seconds = (wake_up_time - sleep_time).to_id
+  end
+end
