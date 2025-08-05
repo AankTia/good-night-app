15b8f2f3d6fc903b186368c0427390fc50c50aa5
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 15:09:51 +0700

    Initial spec rails_helper

---
+++ spec/rails_helper.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require_relative '../config/environment'
+abort('The Rails environment is running in production mode!') if ENV['RAILS_ENV'] Rails.env.production?
+
+require 'rspec/rails'
+require 'factory_bot_rails'
+require 'database_cleaner/active_record'
+
+Rspec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config_around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
