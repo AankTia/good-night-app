1c2643c6957c2df47797d33844aaf67c6713416c
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 14:52:40 +0700

    Initial User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy
+  has_many :followings, class_name: 'UserFollowing', foreign_key: 'follower_id', dependent: :destroy
+  has_mamy :followers, class_name: 'UserFollowing', foreign_key: 'Following_id', dependent: :destroy
+
+  has_many :following_users, through: :followings, source: :following
+  has_many :follower_users, through: :followers, source: :follower
+
+  validates :name, presence: true, length: { maximum: 100 }
+end
