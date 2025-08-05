1c373205847178ff6b4d8be71f9d925b77d4032d
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 15:26:01 +0700

    Add User model validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe User, type: :model do
     it { should have_many(:following_users).through(:followings) }
     it { should have_many(:follower_users).through(:followers) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(100) }
+  end
 end
