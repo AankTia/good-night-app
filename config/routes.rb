4d87f18fb1576b65a2b118704abcf17ab61b0872
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 22:13:19 +0700

    Initial routes for sleep records

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show] do
+        resources :sleep_records, only: [:index, :create, :show] do
+        end
+      end
+    end
+  end
 end
