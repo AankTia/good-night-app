bb47602992a7b264c929661415e7779615a6efa9
TiaWidi <tia.septianawidi@gmail.com>
2025-08-05 15:02:04 +0700

    Initial UserFollowing model

---
+++ app/models/user_following.rb
@@ -0,0 +1,17 @@
+class UserFollowing < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :following, class_name: 'User'
+
+  validates :follower_id, uniqueness: { scope: :following_id }
+  validate :cannot_follow_self
+
+  private
+
+  def cannot_follow_self
+    return unless follower_id && following_id
+
+    if follower_id == following_id
+      errors.add(:following_id: 'cannot follow yourself')
+    end
+  end
+end
