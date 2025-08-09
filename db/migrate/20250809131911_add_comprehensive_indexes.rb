86141596080c067af3ff6d86fada926ec8c3a94a
TiaWidi <tia.septianawidi@gmail.com>
2025-08-09 20:42:44 +0700

    Add command for Remove all custome indexes

--- db/migrate/20250809131911_add_comprehensive_indexes.rb
+++ db/migrate/20250809131911_add_comprehensive_indexes.rb
@@ -56,4 +56,20 @@ class AddComprehensiveIndexes < ActiveRecord::Migration[7.1]
               [:follower_id, :created_at],
               name: 'idx_user_followings_time'
   end
+
+  def down
+    # Remove all custome indexes
+    execute "DROP INDEX IF EXISTS idx_sleep_records_user_time_duration"
+    execute "DROP INDEX IF EXISTS idx_active_sleep_records"
+    execute "DROP INDEX IF EXISTS idx_completed_sleep_records"
+    execute "DROP INDEX IF EXISTS idx_sleep_records_time_user"
+    execute "DROP INDEX IF EXISTS idx_sleep_records_duration_analysis"
+    execute "DROP INDEX IF EXISTS idx_user_followings_reverse"
+    execute "DROP INDEX IF EXISTS idx_user_followings_time"
+    execute "DROP INDEX IF EXISTS idx_users_name_lower"
+    execute "DROP INDEX IF EXISTS idx_users_activity"
+
+    remove_index :user_followings, name: 'idx_user_followings_unique'
+    add_index :user_followings, [:follower_id, :following_id], unique: true
+  end
 end
