544157284823b5c74ed350be55743b9c53a3e617
TiaWidi <tia.septianawidi@gmail.com>
2025-08-14 21:14:41 +0700

    Follow & unfollow api doc

--- spec/requests/api/user_followings_spec.rb
+++ spec/requests/api/user_followings_spec.rb
@@ -30,4 +30,55 @@ RSpec.describe 'user_followings', type: :request do
       end
     end
   end
+
+  path '/api/v1/users/{user_id}/followings' do
+    post 'Create a following' do
+      tags 'User Followings'
+      produces 'application/json'
+      parameter name: :user_id, in: :path, type: :integer, description: 'ID of the user'
+      parameter name: :following_id, in: :query, type: :integer, description: 'ID of the user to follow'
+
+      response '201', 'Following created' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 follower_id: { type: :integer },
+                 following_id: { type: :integer },
+                 created_at: { type: :string, format: 'date-time' }
+               },
+               required: ['id', 'follower_id', 'following_id', 'created_at']
+
+        let(:user_id) { 1 }
+        let(:following_id) { 2 } # Assuming this user exists
+        run_test!
+      end
+
+      response '422', 'Invalid parameters' do
+        let(:user_id) { 1 }
+        let(:following_id) { nil } # Missing following_id
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{user_id}/followings/{target_user_id}' do
+    delete 'Unfollow a user' do
+      tags 'User Followings'
+      produces 'application/json'
+      parameter name: :user_id, in: :path, type: :integer, description: 'ID of the user'
+      parameter name: :target_user_id, in: :path, type: :integer, description: 'ID of the user to unfollow'
+
+      response '204', 'Successfully unfollowed' do
+        let(:user_id) { 1 }
+        let(:target_user_id) { 2 } # Assuming this user is being followed
+        run_test!
+      end
+
+      response '404', 'Following not found' do
+        let(:user_id) { 1 }
+        let(:target_user_id) { 9999 } # Assuming this user is not being followed
+        run_test!
+      end
+    end
+  end
 end
